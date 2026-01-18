851b010b951c41f1882d08c89670c7a22eef4198
Jarrett <blackson.jarrett@gmail.com>
2026-01-17 17:47:02 -0700

    Bs comit

--- frontend/cs2front/src/App.tsx
+++ frontend/cs2front/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is new count{count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
